7538901649e695e8fc6733849ba4eb5198e03074
wellington_uczak <guilherme.uczak@gruporbs.com.br>
2022-06-13 19:41:30 -0300

    commit

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,5 +1,7 @@
 #import os
 from random import randint
+#from time import time
+import time
 
 # print('foi')
 
@@ -14,7 +16,7 @@ CUT_NUMBER = NUMBER_OF_ENTRIES/2  # numero de corte para gerar nova populacao
 
 
 class objects(object):
-    def __init__(self, point_x, point_y, chromosome, fun, weighting=0):
+    def __init__(self, point_x=0, point_y=0, chromosome=0, fun=0, weighting=0):
         self.__point_x = point_x
         self.__point_y = point_y
         self.__chromosome = chromosome
@@ -36,14 +38,26 @@ class objects(object):
     def get_chromosome(self):
         return self.__chromosome
 
+    def set_chromosome(self, chromosome):
+        self.__chromosome = chromosome
+
     def get_fun(self):
         return self.__fun
+
+    def set_fun(self, fun):
+        self.__fun = fun
 
     def get_weighting(self):
         return self.__weighting
 
     def set_weighting(self, weighting):
         self.__weighting = weighting
+    
+    def convert_chromosome(self):
+        point_x =  str(self.__chromosome[0:3])
+        point_y =  str(self.__chromosome[3:5])
+        self.__point_x = int(point_x, 2)
+        self.__point_y = int(point_y, 2)
 
 
 def convert_integer_to_binary(integer):
@@ -138,24 +152,144 @@ def __add_weighting(stap_six):
         print(obj.print_yes_weighting())
     return new_object_of_table
 
-
-def __stap_seven(stap_six):
-    new_object_of_table = __add_weighting(stap_six)
-    number = CUT_NUMBER
-    while number >= 0:
-        new_object_of_table.append
-        number = number - 1
-
-
 def sort_number_of_chromosomes():
-    number = randint(1, 6)
-    print('quantidade de cromossomos que seram utilizados para mutação: ', number)
-    direction = randint(0, 1)
+    amount = 2#randint(1, 5)
+    print('quantidade de cromossomos que seram utilizados para mutação: ', amount)
+    direction = 1#randint(0, 1)
     if direction == 1:
         print('começando a mutação de tras para a frente')
     else:
         print('começando a mutação da frente para tras')
+    return amount, direction
 
+
+def replicate_population(individual,  amount, direction):
+    replicate_population =[]
+    replicate_population.append(individual) # salvando o pai na nova lista
+    chromosome = str(individual.get_chromosome())
+    chromosome_changed = ''
+    chromosome_unchanged = chromosome
+    new_chromosome = chromosome
+    if direction == 0:
+        chromosome_changed = chromosome[0:amount]
+        chromosome_unchanged = chromosome[amount:len(chromosome)]
+        new_chromosome = chromosome_convert(chromosome_changed) + chromosome_unchanged 
+    else: #if direction == 1:
+        chromosome_changed = chromosome[(len(chromosome) - amount):len(chromosome)]
+        chromosome_unchanged = chromosome[0:(len(chromosome) - amount)]
+        new_chromosome = chromosome_unchanged + chromosome_convert(chromosome_changed)
+
+    #print('chromosome_changed', chromosome_changed)
+    #print('chromosome_unchanged', chromosome_unchanged)
+    #print('new_chromosome', new_chromosome)
+    
+    iteration = 0
+    while iteration < individual.get_weighting() - 1:
+        if direction == 0:
+            new_chromosome = chromosome_convert(chromosome_changed) + chromosome_unchanged 
+        else:
+            new_chromosome = chromosome_unchanged + chromosome_convert(chromosome_changed)
+        #for pop in amount_individual:
+        replicate_population.append(objects(chromosome=new_chromosome))
+        iteration+=1
+    ##print('test')
+    #print(replicate_population.print_yes_weighting())
+    return replicate_population
+
+def chromosome_convert(chromosome):
+    returned = ''
+    for chr in chromosome:
+        #time.sleep(1.5)
+        # num = randint(0, 100)
+        # if num % 2 == 0:
+        #     returned = returned + '0'
+        # else:
+        #     returned = returned + '1'
+        returned = returned + str(randint(0, 1))
+        #print(chr)
+        # if chr =='0':
+        #     returned = returned + '1'
+        # else:
+        #     returned = returned + '0'
+    return returned
+
+def create_new_population(object_with_weighting, amount, direction):
+    children = []
+    for individual in object_with_weighting:
+        children = children + replicate_population(individual, amount, direction)
+    return children
+
+def __stap_seven(stap_six):
+    number = CUT_NUMBER
+    object_with_weighting = __add_weighting(stap_six)
+    amount, direction = sort_number_of_chromosomes()
+    new_population = create_new_population(object_with_weighting, amount, direction) 
+    
+    print('\n passo 7')
+    print('X | Y | Cromossomo | f(x,y) | Ponderação')
+    for ff in new_population:
+        print(ff.print_yes_weighting())
+    # while number >= 0:
+    #     new_object_of_table.append
+    #     number = number - 1
+    return new_population
+
+
+
+def __stap_nine(stap_seven):
+
+    individual_position = []
+    while True:
+
+        num = str(randint(0, 9))
+        if num not in individual_position:
+            individual_position.append(num)
+            if len(individual_position) == 5:
+                break
+
+    # for ff in individual_position:
+    #     print(ff)
+
+    for position in individual_position:
+        individual = stap_seven[int(position)]
+        chromosome = individual.get_chromosome()
+        num = randint(0, 5)
+        print('chromosome', int(position) + 1 , 'gene',num +1)
+        #print('num',num)
+        chromosome_changed_pre = chromosome[0:num]
+        chromosome_changed_pos = chromosome[num + 1:len(chromosome)]
+        chromosome_unchanged = chromosome[num]
+        if chromosome_unchanged == '1':
+            chromosome_unchanged = '0'
+        else:
+            chromosome_unchanged = '1'
+        print('chromosome_changed_pre', chromosome_changed_pre , 'chromosome_unchanged',chromosome_unchanged, 'chromosome_changed_pos',chromosome_changed_pos)
+        
+        individual.set_chromosome(chromosome_changed_pre + chromosome_unchanged + chromosome_changed_pos)
+
+    print('\n passo 9')
+    print('X | Y | Cromossomo | f(x,y) | Ponderação')
+    for ff in stap_seven:
+        print(ff.print_yes_weighting())
+
+
+def __stap_ten(stap_seven):
+    for individual in stap_seven:
+        individual.convert_chromosome()
+
+    print('\n passo 10')
+    print('X | Y | Cromossomo | f(x,y) | Ponderação')
+    for ff in stap_seven:
+        print(ff.print_yes_weighting())
+
+def __stap_eleven(stap_seven):
+    for individual in stap_seven:
+        individual.set_fun(function(individual.get_point_x(), individual.get_point_y()))
+
+    print('\n passo 11')
+    print('X | Y | Cromossomo | f(x,y) | Ponderação')
+    for ff in stap_seven:
+        print(ff.print_yes_weighting())
 
 # stapTwo
 __stap_two()
@@ -175,7 +309,9 @@ stap_six = __stap_six(stap_five)
 # stapSeven
 stap_seven = __stap_seven(stap_six)
 # sort number of chromosomes for mutation
-sort_number_of_chromosomes()
-#MAX_VALUE = int(stap_six[0].get_fun())
+__stap_nine(stap_seven)
 
+stap_ten = __stap_ten(stap_seven)
+
+stap_eleven = __stap_eleven(stap_seven)
 # print(MAX_VALUE)
